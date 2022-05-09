2d292b7379e4344fee9cc9bfe107c132b4d64672
sqwot <sqwot@mail.ru>
2022-05-09 21:21:19 +0000

    Add web app

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	logrus "github.com/sirupsen/logrus"
@@ -9,10 +10,10 @@ import (
 
 func main() {
 	logrus.Info("HelloWorld!!")
-	logrus.Warning("Warning")
-	logrus.Error("Error")
-	//logrus.Fatal("Fatal")
-	logrus.Debug("debug")
+	// logrus.Warning("Warning")
+	// logrus.Error("Error")
+	// logrus.Fatal("Fatal")
+	// logrus.Debug("debug")
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
@@ -20,4 +21,9 @@ func main() {
 	}
 	logrus.Info(fmt.Sprintf("Приложение будет работать на %s порту", port))
 
+	http.ListenAndServe(":"+port, nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello"))
+	})
 }
