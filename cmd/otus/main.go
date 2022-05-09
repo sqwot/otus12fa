10fd837d33b0d70ba87184dab9de8279d8794131
sqwot <sqwot@mail.ru>
2022-05-09 20:30:50 +0000

    add logger

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	logrus "github.com/sirupsen/logrus"
+)
+
 func main() {
+	logrus.Info("HelloWorld!!")
+	logrus.Warning("Warning")
+	logrus.Error("Error")
+	logrus.Fatal("Fatal")
+	logrus.Debug("debug")
 
 }
