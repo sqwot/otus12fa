865f1eebe40c8e9fa6a366091075b5950aefd3a0
sqwot <sqwot@mail.ru>
2022-05-09 20:47:41 +0000

    Add env configuration

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	logrus "github.com/sirupsen/logrus"
 )
 
@@ -8,7 +11,13 @@ func main() {
 	logrus.Info("HelloWorld!!")
 	logrus.Warning("Warning")
 	logrus.Error("Error")
-	logrus.Fatal("Fatal")
+	//logrus.Fatal("Fatal")
 	logrus.Debug("debug")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logrus.Fatal("Не указан порт для работы веб-приложения")
+	}
+	logrus.Info(fmt.Sprintf("Приложение будет работать на %s порту", port))
 
 }
