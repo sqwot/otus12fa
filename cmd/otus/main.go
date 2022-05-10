5dc280ed13bd9e417c7e362312c3c4022e1f405b
sqwot <sqwot@mail.ru>
2022-05-10 08:36:15 +0000

    gracefull shutdown

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	mux "github.com/gorilla/mux"
 
 	logrus "github.com/sirupsen/logrus"
 )
@@ -21,9 +28,25 @@ func main() {
 	}
 	logrus.Info(fmt.Sprintf("Приложение будет работать на %s порту", port))
 
-	http.ListenAndServe(":"+port, nil)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello"))
 	})
+
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
+	}
+	go serv.ListenAndServe()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+	timeout, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	if err := serv.Shutdown(timeout); err != nil {
+		logrus.Fatal("Не удалось корректно завершить работу веб-сервера")
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/sqwot/otus12fa
 
 go 1.18
 
-require github.com/sirupsen/logrus v1.8.1
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.8.1
+)
 
 require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
